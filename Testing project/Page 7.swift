804fd4c0ab7f99edd649d179af3024bec77d0463
Jon Thacker <jon.thacker.jr@gmail.com>
2019-09-13 13:13:51 -0600

    Testing

---
+++ Testing project/Page 7.swift
@@ -0,0 +1,30 @@
+//
+//  Page 7.swift
+//  Testing project
+//
+//  Created by Jon Thacker on 9/12/19.
+//  Copyright © 2019 Jon Thacker. All rights reserved.
+//
+
+import UIKit
+
+class Page_7: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Testing project/Table of Contents.swift
@@ -0,0 +1,30 @@
+//
+//  Table of Contents.swift
+//  Testing project
+//
+//  Created by Jon Thacker on 9/12/19.
+//  Copyright © 2019 Jon Thacker. All rights reserved.
+//
+
+import UIKit
+
+class Table_of_Contents: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
